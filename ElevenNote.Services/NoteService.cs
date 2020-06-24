cd22cd906ba49b414fbad845b1d0121a7773ce6a
STennyson <seth.m.tennyson@gmail.com>
2020-06-24 11:31:57 -0400

    added update and put and edit

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -36,38 +36,53 @@ namespace ElevenNote.Services
 
         public IEnumerable<NoteListItem> GetNotes()
         {
-                var query =
-                    _ctx
-                        .Notes
-                        .Where(e => e.OwnerId == _userId)
-                        .Select(
-                            e =>
-                                new NoteListItem
-                                {
-                                    NoteId = e.NoteId,
-                                    Title = e.Title,
-                                    CreatedUtc = e.CreatedUtc
-                                }
-                        );
+            var query =
+                _ctx
+                    .Notes
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                            new NoteListItem
+                            {
+                                NoteId = e.NoteId,
+                                Title = e.Title,
+                                CreatedUtc = e.CreatedUtc
+                            }
+                    );
 
-                return query.ToArray();
+            return query.ToArray();
         }
 
         public NoteDetail GetNoteById(int id)
         {
-                var entity =
-                    _ctx
-                        .Notes
-                        .Single(e => e.NoteId == id && e.OwnerId == _userId);
-                return
-                    new NoteDetail
-                    {
-                        NoteId = entity.NoteId,
-                        Title = entity.Title,
-                        Content = entity.Content,
-                        CreatedUtc = entity.CreatedUtc,
-                        ModifiedUtc = entity.ModifiedUtc
-                    };
+            var entity =
+                _ctx
+                    .Notes
+                    .Single(e => e.NoteId == id && e.OwnerId == _userId);
+            return
+                new NoteDetail
+                {
+                    NoteId = entity.NoteId,
+                    Title = entity.Title,
+                    Content = entity.Content,
+                    CreatedUtc = entity.CreatedUtc,
+                    ModifiedUtc = entity.ModifiedUtc
+                };
+        }
+
+        public bool UpdateNote(NoteEdit model)
+        {
+
+            var entity =
+                _ctx
+                    .Notes
+                    .Single(e => e.NoteId == model.NoteId && e.OwnerId == _userId);
+
+            entity.Title = model.Title;
+            entity.Content = model.Content;
+            entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+            return _ctx.SaveChanges() == 1;
         }
     }
 }
